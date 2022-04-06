abfa8dcb89af27c0a9739a5118f109d23f1e24bd
Sanghyun Ryu <ricky4918@gmail.com>
2022-04-06 14:05:20 -0700

    Create Longest Consecutive Sequence.py

---
+++ Longest Consecutive Sequence.py
@@ -0,0 +1,33 @@
+# [59, 12, 25, 11]
+# return the length of the logest consecutive lelements
+# o(n)
+
+
+
+
+
+class Solution:
+    def longestConsecutive(self, nums):
+        
+        if not nums:
+            return 0
+        
+        nums.sort()
+        
+        #[11,12,25,59]
+        
+        longest_streak = 1
+        current_streak = 1
+        
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i-1]:
+                if nums[i] == nums[i-1]+1:
+                    current_streak+=1
+            
+            else:
+                longest_streak = max(longest_streak, current_streak)
+                current_streak = 1
+                
+        return max(longest_streak, current_streak)
+        
+        
