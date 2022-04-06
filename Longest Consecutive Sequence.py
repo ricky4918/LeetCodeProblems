a103d3ea48d655bec636a6586dc8610cdee18a11
Sanghyun Ryu <ricky4918@gmail.com>
2022-04-06 14:06:07 -0700

    Update Longest Consecutive Sequence.py

--- Longest Consecutive Sequence.py
+++ Longest Consecutive Sequence.py
@@ -8,26 +8,22 @@
 
 class Solution:
     def longestConsecutive(self, nums):
-        
         if not nums:
             return 0
-        
+
         nums.sort()
-        
-        #[11,12,25,59]
-        
+
         longest_streak = 1
         current_streak = 1
-        
+
         for i in range(1, len(nums)):
             if nums[i] != nums[i-1]:
                 if nums[i] == nums[i-1]+1:
-                    current_streak+=1
-            
-            else:
-                longest_streak = max(longest_streak, current_streak)
-                current_streak = 1
-                
+                    current_streak += 1
+                else:
+                    longest_streak = max(longest_streak, current_streak)
+                    current_streak = 1
+
         return max(longest_streak, current_streak)
         
         
