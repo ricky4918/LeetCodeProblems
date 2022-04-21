4a1ca5dc806e2eed148df66df1e12254f63ba63a
Sanghyun Ryu <ricky4918@gmail.com>
2022-04-20 22:58:57 -0700

    Nutanix interview questions

--- Nutanix_Problems/Serialize and Deserialize Binary Tree.py
+++ Nutanix_Problems/Serialize and Deserialize Binary Tree.py
@@ -7,38 +7,55 @@
 
 class Codec:
 
-
     def serialize(self, root):
-
-
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
         res = []
-
+        
         def dfs(node):
+            
             if not node:
-                res.append('N')
+                res.append("N")
                 return
-            res.append(str(node.val))
-
+            else:
+                res.append(str(node.val))
+                
             dfs(node.left)
             dfs(node.right)
         
         dfs(root)
         return ",".join(res)
+        
 
     def deserialize(self, data):
-
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
         vals = data.split(",")
-
         self.i = 0
-
+        
         def dfs():
-
-            if vals[self.i] == 'N':
+     
+            if vals[self.i] == "N":
                 self.i += 1
                 return None
-            node = TreeNode(int(vals[self.i]))
-            self.i +=1
+            else:
+                node = TreeNode(int(vals[self.i]))
+                self.i += 1
+            
             node.left = dfs()
             node.right = dfs()
             return node
+        
         return dfs()
+        
+
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# ans = deser.deserialize(ser.serialize(root))
