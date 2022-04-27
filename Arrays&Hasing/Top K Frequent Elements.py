108a21ce0eb6f42176fabce05999e59fbd7481da
ricky4918 <ricky4918@gmail.com>
2022-04-27 16:13:47 -0700

    commit

--- Arrays&Hasing/Top K Frequent Elements.py
+++ Arrays&Hasing/Top K Frequent Elements.py
@@ -1,15 +1,31 @@
 from collections import Counter
-import heapq
 
 
 class Solution:
-    def topKFrequent(self, nums, k):
-        
-       
-        #maxheap
-        #pop k times
 
+    def topKFrequent(self, nums, k):
 
         #bucket sort
 
+        count = {}
+        freq = [[] for _ in range(len(nums) + 1)]
+
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+
+        for n, c in count.items():
+            freq[c].append(n)
+
         
+        res = []
+        for i in range(len(freq)-1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) ==k:
+                    return res
+
+
+p = Solution()
+nums = [1,1,1,2,2,5,5,5,5,5,5,5,3]
+k = 2
+print(p.topKFrequent(nums,k))
