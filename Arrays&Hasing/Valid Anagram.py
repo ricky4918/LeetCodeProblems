54d0e96aa3a3b951ab019dcaa57bdbe68b420500
ricky4918 <ricky4918@gmail.com>
2022-04-27 14:34:41 -0700

    commit Leetcode 242

--- Arrays&Hasing/Valid Anagram.py
+++ Arrays&Hasing/Valid Anagram.py
@@ -1,16 +1,33 @@
+import collections
 class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
+    def isAnagram(self, s: str, t: str) -> bool: # time O(n),  space O(s+t)
         if len(s) != len(t):
             return False
-        
+
         countS, countT = {}, {}
-        
+
+
         for i in range(len(s)):
-            countS[s[i]] = 1 + countS.get(s[i], 0)
-            countT[t[i]] = 1 + countT.get(t[i], 0)
+
+            countS[s[i]] = 1 + countS.get(s[i],0)
+            countT[t[i]] = 1 + countT.get(t[i],0)
+        
         for c in countS:
-            if countS[c] != countT.get(c, 0):
+            if countS[c] != countT[c]:
                 return False
-        return True
-    
+
+            else:
+                return True
+
+    def isAnagramCheating(self, s, t):
+        return collections.Counter(s) == collections.Counter(t)
+
+    def isAnagram2(self, s, t):
+        return sorted(s) == sorted(t)
   
+s = "anagram"
+t = "nagaram"
+res = Solution()
+print(res.isAnagram(s,t))
+print(res.isAnagramCheating(s,t))
+print(res.isAnagram2(s,t))
