5727105d26d87af89718133d9690c2c82f0748b3
ricky4918 <ricky4918@gmail.com>
2022-04-27 13:23:48 -0700

    commit

--- Arrays&Hasing/Contains Duplicate.py
+++ Arrays&Hasing/Contains Duplicate.py
@@ -1,4 +1,26 @@
 class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
+
+    def containsDuplicate1(self, nums):
         nums.sort()
         return any([nums[i] == nums[i+1] for i in range(len(nums)-1)])
+
+    def containsDuplicate2(self, nums):
+        
+
+        hashset = set()
+
+        for n in nums:
+
+            if n in hashset:
+                return True
+            
+            else:
+                hashset.add(n)
+        return False
+
+
+if __name__ == "__main__":
+
+    nums = [1,2,3,1]
+    res = Solution()
+    print(res.containsDuplicate2(nums))
