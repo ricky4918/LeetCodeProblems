53fd0ab7236ae7dcf277eb38b8e177d6e56189e8
ricky4918 <ricky4918@gmail.com>
2022-05-02 09:35:25 -0700

    added Leetcode #36

--- Arrays&Hasing/Valid Sudoku.py
+++ Arrays&Hasing/Valid Sudoku.py
@@ -1,39 +1,40 @@
 #HashSet
 #o(n^2)
-    
-    
-    #   0     1     2
-#     1 2 3 4 5 6 7 8 9
-#   1
-# 0 2
-#   3
-#   4
-# 1 5
-#   6
-#   7
-# 2 8
-#   9
-        
- 
+import collections
 class Solution:
     def isValidSudoku(self, board):
-        
-        
+
+#hashset to check the row and column nust contain the digits 1-9 without prepetition
+
         cols = collections.defaultdict(set)
         rows = collections.defaultdict(set)
-        squares = collections.defaultdict(set) # key = (r/3, c/3)
-        
-        
+        square = collections.defaultdict(set) # key = (r/3, c/3)
+
+
         for r in range(9):
             for c in range(9):
                 if board[r][c] == ".":
                     continue
-                if(board[r][c] in rows[r] or 
-                   board[r][c] in cols[c] or
-                   board[r][c] in squares[(r//3,c//3)]):
+                if (board[r][c] in rows[r] or
+                    board[r][c] in cols[r] or
+                    board[r][c] in square[(r//3, c//3)]):
                     return False
-                rows[r].add(board[r][c])
                 cols[c].add(board[r][c])
-                squares[(r//3,c//3)].add(board[r][c])
+                rows[r].add(board[r][c])
+                square[(r//3,c//3)].add(board[r][c])
+        
         return True
-                   
+
+
+p = Solution()
+board = [["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+
+print(p.isValidSudoku(board))
